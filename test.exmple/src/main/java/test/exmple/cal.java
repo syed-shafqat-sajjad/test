8d1d4095c844a54733e7c04a860e09535cae40a8
SyedShafqatSajjad <SyedShafqatSajjad@DESKTOP-GSU2TD3>
2020-01-03 12:05:48 +0500

    2nd change updated

---
+++ test.exmple/src/main/java/test/exmple/cal.java
@@ -0,0 +1,12 @@
+package test.exmple;
+
+public class cal {
+
+	public int add(int a , int b)
+	{
+		
+		return a+b;
+	}
+	
+	
+}
---
+++ test.exmple/src/test/java/test/exmple/addtestcase.java
@@ -0,0 +1,14 @@
+package test.exmple;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class addtestcase {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
